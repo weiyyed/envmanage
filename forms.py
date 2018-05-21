5414720d2edad03ef90894925698b5966ff6774a
weiyyed <weiyyed@163.com>
2018-05-21 23:11:19 +0800

    调通页面

--- forms.py
+++ forms.py
@@ -2,8 +2,9 @@ from flask_wtf import FlaskForm
 from wtforms import BooleanField,SubmitField
 
 class Restart(FlaskForm):
-    # server_names=['sy','mgr','portal']
-    # for i in server_names:
-    sy=BooleanField('sy')
-    mgr=BooleanField('mgr')
-    restart=SubmitField('重启')
+    server_names=['sy','mgr','portal','passport','eam','trn','hse','csc','phd','pom',
+                  'eam_m','trn_m','hse_m','csc_m','phd_m','pom_m']
+
+    for i in server_names:
+        locals()[i]=BooleanField(i)
+        restart=SubmitField('重启')
